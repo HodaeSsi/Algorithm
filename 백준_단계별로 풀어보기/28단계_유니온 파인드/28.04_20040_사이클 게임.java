5833d8b51b33885ceddd8d82c679f99d11e9a7ae
HodaeSsi <kfdream123@gmail.com>
2021-02-18 22:55:51 +0900

    21/02/18

---
+++ 백준_단계별로 풀어보기/28단계_유니온 파인드/28.04_20040_사이클 게임.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static int parent[];
+
+    public static int find(int x) {
+        if(parent[x] < 0)
+            return x;
+        else {
+            int y = find(parent[x]);
+            parent[x] = y;
+            return y;
+        }
+    }
+
+    public static boolean union(int x, int y) {
+        int X = find(x);
+        int Y = find(y);
+        if(X == Y)
+            return false;
+        if(parent[X] < parent[Y]) {
+            int tmp = parent[Y];
+            parent[Y] = X;
+            parent[X] += tmp;
+        } else {
+            int tmp = parent[X];
+            parent[X] = Y;
+            parent[Y] += tmp;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] readLine = br.readLine().split(" ");
+        int n = Integer.parseInt(readLine[0]);
+        parent = new int[n];
+        for(int i = 0; i < n; i++)
+            parent[i] = -1;
+        int m = Integer.parseInt(readLine[1]);
+
+        for(int i = 0; i < m; i++) {
+            readLine = br.readLine().split(" ");
+            int a = Integer.parseInt(readLine[0]);
+            int b = Integer.parseInt(readLine[1]);
+            if(!union(a, b)) {
+                System.out.println(i + 1);
+                return;
+            }
+        }
+        System.out.println(0);
+    }
+}
