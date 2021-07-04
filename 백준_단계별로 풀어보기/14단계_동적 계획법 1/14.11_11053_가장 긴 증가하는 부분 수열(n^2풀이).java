f81ea662b5bb42e813d19da6b753b8c36b72861f
HodaeSsi <kfdream123@gmail.com>
2021-07-04 21:37:02 +0900

    21-07-04

--- 백준_단계별로 풀어보기/14단계_동적 계획법 1/14.11_11053_가장 긴 증가하는 부분 수열(n^2풀이).java
+++ 백준_단계별로 풀어보기/14단계_동적 계획법 1/14.11_11053_가장 긴 증가하는 부분 수열(n^2풀이).java
@@ -6,6 +6,7 @@ public class Main{
     public static ArrayList<Integer> DP = new ArrayList<Integer>();
 
     public static void main(String args[]){
+
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] n = new int[N];
--- 백준_단계별로 풀어보기/20단계_이분 탐색/20.01_1920_수 찾기(re2).java
+++ 백준_단계별로 풀어보기/20단계_이분 탐색/20.01_1920_수 찾기(re2).java
@@ -29,6 +29,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         n = new int[N];
